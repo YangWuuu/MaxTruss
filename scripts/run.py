b1415a578551ef46a015773cdb70623252c0adf6
Yang <1208490616@qq.com>
2020-11-19 00:01:40 +0800

    fix a bug about action

--- scripts/run.py
+++ scripts/run.py
@@ -16,6 +16,14 @@ data_and_results = [
 ]
 
 
+def recu_down(url, filename):
+    try:
+        urllib.request.urlretrieve(url, filename)
+    except urllib.error.ContentTooShortError:
+        print('Network conditions is not good. Reloading...')
+        recu_down(url, filename)
+
+
 def download():
     os.chdir(project_folder)
     url = "http://datafountain.int-yt.com/Files/BDCI2020/473HuaKeDaKtruss/ktruss-data.zip"
@@ -25,7 +33,7 @@ def download():
         if not os.path.exists(zip_file_path):
             download_start = time.time()
             print("start download")
-            urllib.request.urlretrieve(url, zip_file_path)
+            recu_down(url, zip_file_path)
             print("download cost: {}s".format(int(time.time() - download_start)))
         with zipfile.ZipFile(zip_file_path) as f:
             f.extractall(path=data_folder)
@@ -59,13 +67,16 @@ def time_analysis(cmd, data_path, result, count=1, para=""):
 
 
 def main():
-    cmds = ["kmax_truss", "kmax_truss_serial"]
+    cmds = [
+        ["kmax_truss", 11],
+        ["kmax_truss_serial", 3]
+    ]
     print("project_folder: {}".format(project_folder))
     os.chdir(project_folder)
     download()
     for cmd in cmds:
-        if os.path.exists(cmd):
-            os.remove(cmd)
+        if os.path.exists(cmd[0]):
+            os.remove(cmd[0])
     os.system("make")
     start = time.time()
     for cmd in cmds:
@@ -74,8 +85,8 @@ def main():
             if not os.path.exists(data_file):
                 print("file is not exist: {}".format(data_file))
                 continue
-            min_t, _, _, mid_t = time_analysis(cmd, data_file, d[1])
-            print("{:^20} {:^30} {:.0f}ms {:.0f}ms".format(cmd, d[0], min_t, mid_t))
+            min_t, _, _, mid_t = time_analysis(cmd[0], data_file, d[1], cmd[1])
+            print("{:^20} {:^30} {:.0f}ms {:.0f}ms".format(cmd[0], d[0], min_t, mid_t))
     print("run cost: {}s".format(int(time.time() - start)))
 
 
