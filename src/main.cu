27956883d53a5bced417dd21afc8dd049e6d3e62
Yang <1208490616@qq.com>
2020-11-19 23:56:00 +0800

    test cuda

--- src/main.cu
+++ src/main.cu
@@ -1,6 +1,13 @@
-#include <iostream>
+#include "log.h"
 
 __global__ void add(int a, int b, int *c) { *c = a + b; }
+
+void init_gpu() {
+  cudaDeviceProp deviceProp{};
+  cudaGetDeviceProperties(&deviceProp, 0);
+  uint64_t gpu_mem = deviceProp.totalGlobalMem;
+  log_info("Mem: %uMB", gpu_mem / 1024 / 1024);
+}
 
 int main() {
   int c;
@@ -8,7 +15,8 @@ int main() {
   cudaMalloc((void **)&dev_c, sizeof(int));
   add<<<1, 1>>>(2, 7, dev_c);
   cudaMemcpy(&c, dev_c, sizeof(int), cudaMemcpyDeviceToHost);
-  std::cout << "2 + 7 = " << c << std::endl;
+  log_info("2 + 7 = %d", c);
   cudaFree(dev_c);
+  init_gpu();
   return 0;
 }
